557318923325220e55b2a43ac731eb1182bae85c
shulgatyy <shulgatyy@users.noreply.github.com>
2018-03-24 23:31:20 +0200

    now render clear children before mount; safe style prop; complete class, for and dataset attrs

--- fake-react.js
+++ fake-react.js
@@ -1,15 +1,12 @@
 function render(element, mountNode) {
+	while (mountNode.lastChild) mountNode.removeChild(mountNode.lastChild);
 	mountNode.appendChild(element);
 }
 
 function createElement(name, props = {}, children) {
 	const element = document.createElement(name);
 
-	Object.keys(props).forEach(propName => {
-		// TODO "class" and "for" attrs
-		if (propName === "style") Object.assign(element.style, props.style);
-		else element[propName] = props[propName];
-	});
+	Object.entries(props).forEach(prop => addProp(element, prop));
 
 	if (!children) return element;
 	const append = child => element.append(child);
@@ -17,4 +14,32 @@ function createElement(name, props = {}, children) {
 	else append(children);
 
 	return element;
+}
+
+function addProp(element, [propName, propVal]) {
+	switch (propName) {
+		case "style":
+		case "dataset":
+			if (
+				propVal &&
+				propVal.constructor &&
+				propVal.constructor === Object
+			)
+				Object.assign(element[propName], propVal);
+			break;
+		case "for":
+			element.htmlFor = propVal;
+			break;
+		case "class":
+			const classes = propVal;
+			if (Array.isArray(classes)) {
+				if (classes.length) element.classList.add(...classes);
+				// element.className = classes.join(' ');
+			} else {
+				element.className = classes;
+			}
+			break;
+		default:
+			element[propName] = propVal;
+	}
 }
